8e33312654b925e575c44416fb234ea6ffc46c77
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-16 17:22:28 +0900

    Some arguments passed to sphttp's constructor made it possible to specify from URL query

--- app/main.py
+++ app/main.py
@@ -1,17 +1,43 @@
 from flask import Flask, Response, request
 
-from sphttp import Downloader
+from sphttp import Downloader, DelayRequestAlgorithm, DuplicateRequestAlgorithm
 
 app = Flask(__name__)
 
 
 @app.route('/proxy')
 def proxy():
-    string = request.args['host']
-    hosts = string.split(',')
-    d = Downloader(urls=hosts)
-    gen = d.generator()
-    return Response(gen, mimetype='application/octet-stream')
+
+    hosts = request.args.get('hosts').split(',')
+    split_size = int(request.args.get('split_size', 10**6))
+    enable_trace_log = bool(request.args.get('enable_trace_log', False))
+    verify = bool(request.args.get('verify', 'true'))
+    delay_req_algo = eval(request.args.get('delay_req_algo',
+                                           'DelayRequestAlgorithm.DIFF'))
+    enable_dup_req = bool(request.args.get('enable_dup_req', True))
+    dup_req_algo = eval(request.args.get('dup_req_algo',
+                                         'DuplicateRequestAlgorithm.IBRC'))
+    close_bad_conn = bool(request.args.get('close_bad_conn', False))
+    static_delay_req_vals = request.args.get('static_delay_req_vals', None)
+    enable_init_delay = bool(request.args.get('enable_init_delay', True))
+    invalid_block_threshold = int(request.args.get('invalid_block_threshold',
+                                                   20))
+    init_delay_coef = float(request.args.get('init_delay_coef', 10))
+
+    d = Downloader(urls=hosts,
+                   split_size=split_size,
+                   enable_trace_log=enable_trace_log,
+                   verify=verify,
+                   delay_req_algo=delay_req_algo,
+                   enable_dup_req=enable_dup_req,
+                   dup_req_algo=dup_req_algo,
+                   close_bad_conn=close_bad_conn,
+                   static_delay_req_vals=static_delay_req_vals,
+                   enable_init_delay=enable_init_delay,
+                   invalid_block_threshold=invalid_block_threshold,
+                   init_delay_coef=init_delay_coef,)
+
+    return Response(d.generator(), mimetype='application/octet-stream')
 
 
 if __name__ == "__main__":
